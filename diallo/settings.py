2035f26419b6a33cc0ad9ebe3703cb2385c1f266
Bakidjan <55193201+bakidjan@users.noreply.github.com>
2020-11-09 03:52:13 +0100

    Update setting.py, static renamed to templates

--- diallo/settings.py
+++ diallo/settings.py
@@ -117,4 +117,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = './diallo/static/'
+STATIC_URL = './diallo/templates/'
